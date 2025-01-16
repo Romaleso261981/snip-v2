c119fe4ce89aa3939feba8b14a2459ffecc00aa3
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-01-16 12:39:10 +0200

    fix: change project files

--- src/api/majsterkla/controllers/majsterkla.ts
+++
@@ -1,7 +0,0 @@
-/**
- * majsterkla controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::majsterkla.majsterkla');
--- src/api/majsterkla/routes/majsterkla.ts
+++
@@ -1,7 +0,0 @@
-/**
- * majsterkla router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::majsterkla.majsterkla');
